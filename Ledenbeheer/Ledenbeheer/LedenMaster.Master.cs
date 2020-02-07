91c29399cf723e8734932c2e9d375dde41755731
KoenBisschop <koen.bisschop@hotmail.be>
2020-02-07 12:00:52 +0100

    Aanpassen menu: LoginView, LoginStatus, LoginName

--- Ledenbeheer/Ledenbeheer/LedenMaster.Master.cs
+++ Ledenbeheer/Ledenbeheer/LedenMaster.Master.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -16,7 +17,8 @@ namespace Ledenbeheer
 
         protected void LoginStatus1_LoggingOut(object sender, LoginCancelEventArgs e)
         {
-
+            FormsAuthentication.SignOut();
+            Response.Redirect("~/Account/Login.aspx");
         }
     }
 }
--- Ledenbeheer/Ledenbeheer/LedenMaster.Master.designer.cs
+++ Ledenbeheer/Ledenbeheer/LedenMaster.Master.designer.cs
@@ -42,6 +42,15 @@ namespace Ledenbeheer
         protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
